0777454d78f270cab871f0dff2fa72afacbb6a1a
JonasBrosers <105449805+JonasBrosers@users.noreply.github.com>
2022-08-30 14:44:54 -0300

    atualização do frontend

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Front_end.Data;
+using MatBlazor;
+string url = "https://localhost:7294";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMatBlazor();
+builder.Services.AddSingleton<HttpService>(p =>
+{
+    HttpService service = new HttpService(url);
+    return service;
+});
 
 var app = builder.Build();
 
